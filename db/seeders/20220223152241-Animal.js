dcb6af473a24fdd89bc8f98cdf65696ee6b940a5
Artem <artemivlev@yandex.ru>
2022-02-24 14:37:50 +0300

    filled seed with content of animals

--- db/seeders/20220223152241-Animal.js
+++ db/seeders/20220223152241-Animal.js
@@ -12,7 +12,7 @@ module.exports = {
      * }], {});
     */
      await queryInterface.bulkInsert('Animals', [{
-         img: '111.img',
+         img: '../../public/images/waldemar-brandt-zQsCISvBY2E-unsplash.jpeg',
          title: 'Тигр',
          description: 'Амурский',
          user_id: 1,
@@ -20,9 +20,97 @@ module.exports = {
          updatedAt: new Date()
        },
        {
-        img: '111.img',
+       img: '../../public/images/leopard.jpeg',
+       title: 'Леопард',
+       description: 'Кенийский',
+       user_id: 1,
+       createdAt: new Date(),
+       updatedAt: new Date()
+       },
+       {
+         img: '../../public/images/baby_elephant.jpeg',
+         title: 'Слонёнок',
+         description: 'Африканский',
+         user_id: 1,
+         createdAt: new Date(),
+         updatedAt: new Date()
+       },
+       {
+         img: '../../public/images/baby_monkey.jpeg',
+         title: 'Обезьянка',
+         description: 'Милая',
+         user_id: 1,
+         createdAt: new Date(),
+         updatedAt: new Date()
+       },
+       {
+         img: '../../public/images/coala.jpeg',
+         title: 'Коала',
+         description: 'Австралийская',
+         user_id: 1,
+         createdAt: new Date(),
+         updatedAt: new Date()
+       },
+       {
+         img: '../../public/images/crocodile.jpeg',
+         title: 'Крокодил',
+         description: 'Южно - Американский',
+         user_id: 1,
+         createdAt: new Date(),
+         updatedAt: new Date()
+       },
+       {
+         img: '../../public/images/elephants.jpeg',
+         title: 'Слоны',
+         description: 'Африканские',
+         user_id: 1,
+         createdAt: new Date(),
+         updatedAt: new Date()
+       },
+       {
+         img: '../../public/images/monkey_2.jpeg',
+         title: 'Обезьяна',
+         description: 'Узконосная',
+         user_id: 1,
+         createdAt: new Date(),
+         updatedAt: new Date()
+       },
+       {
+         img: '../../public/images/lion.jpeg',
+         title: 'Лев',
+         description: 'Царь',
+         user_id: 1,
+         createdAt: new Date(),
+         updatedAt: new Date()
+       },
+       {
+         img: '../../public/images/owl.jpeg',
+         title: 'Сова',
+         description: 'Привет 3-ей фазе',
+         user_id: 1,
+         createdAt: new Date(),
+         updatedAt: new Date()
+       },
+       {
+         img: '../../public/images/phlamingo.jpeg',
+         title: 'Фламинго',
+         description: 'Розовый фламинго',
+         user_id: 1,
+         createdAt: new Date(),
+         updatedAt: new Date()
+       },
+       {
+         img: '../../public/images/phlamingo.jpeg',
+         title: 'Пингвины',
+         description: 'Те самые ребята из Мадагаскара',
+         user_id: 1,
+         createdAt: new Date(),
+         updatedAt: new Date()
+       },
+       {
+        img: '../../public/images/white_bear.jpeg',
         title: 'Медведь',
-        description: 'Бурый',
+        description: 'Белый',
         user_id: 1,
         createdAt: new Date(),
         updatedAt: new Date()
